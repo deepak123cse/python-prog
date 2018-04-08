2a81afabb2c26a62d110890b8d824bee64045a8e
deepak123cse <deepak123cse@gmail.com>
2018-04-09 01:02:52 +0530

    Exercise 5
    Printing with format strings

---
+++ ex05.py
@@ -0,0 +1,18 @@
+my_name = 'Amigo'
+my_age = 30
+my_height = 170 #Cm
+my_weight = 75 #KGs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print ("Let's talk about %s." % my_name)
+print ("He's %s centimeters tall." % my_height)
+print ("He's %s kilos weight." % my_weight)
+print ("Actually that's not too heavy.")
+print ("He's got %s eyes and %s colored hair." % (my_eyes,my_hair))
+print ("He's got %s teeth." % my_teeth)
+
+#Tricky part
+print ("If i add %d,%d,%d, I'd get %d" % (my_age,my_height,my_weight,my_age+my_height+my_weight))
+
