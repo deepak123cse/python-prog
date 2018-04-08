626ab21eacfb1001c0607552408a1c4665bc0887
deepak123cse <deepak123cse@gmail.com>
2018-04-09 01:25:13 +0530

    Update ex05.py
    Updated code with inches and pound conversions. Working.

--- ex05.py
+++ ex05.py
@@ -2,14 +2,16 @@
 name = 'Amigo'
 age = 30
 height = 170 #Cm
-weight = 75 #KGs
+height_in_inches = height*0.393701
+weight = 88 #KGs
+weight_in_pounds = weight*2.20462
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
 
 print ("Let's talk about %s." % name)
-print ("He's %s centimeters tall." % height)
-print ("He's %s kilos weight." % weight)
+print ("He's %r centimeters tall which is %f inches" % (height,height_in_inches))
+print ("He's %d kilos weight which is %f pounds." % (weight,weight_in_pounds))
 print ("Actually that's not too heavy.")
 print ("He's got %s eyes and %s colored hair." % (eyes,hair))
 print ("He's got %s teeth." % teeth)
